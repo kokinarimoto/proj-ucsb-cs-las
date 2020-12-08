d722ab1a6c6c66ed1ba4b786e8402a0ea0f6ee01
Callter <quansen@ucsb.edu>
2020-12-08 11:18:30 +0800

    qw - add sample files for tutors

---
+++ src/main/java/edu/ucsb/ucsbcslas/repositories/TutorAssignmentRepository.java
@@ -0,0 +1,15 @@
+package edu.ucsb.ucsbcslas.repositories;
+
+import java.util.List;
+
+import edu.ucsb.ucsbcslas.entities.TutorAssignment;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TutorAssignmentRepository extends CrudRepository<TutorAssignment, Long> {
+  List<TutorAssignment> findAll();
+  List<TutorAssignment> findByCourseId(int id);
+  // tutor_assignment_id, tutor_id
+}
